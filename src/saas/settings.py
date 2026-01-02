a58d144bdc77004438a98f3b292117de5288aa52
ahmadbahlool <billoahmad25@gmail.com>
2026-01-02 11:50:17 +0500

    add environment variables using python-decouple

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+print(config('secret_key'))
 SECRET_KEY=config("secret_key",cast="string")
 
 
