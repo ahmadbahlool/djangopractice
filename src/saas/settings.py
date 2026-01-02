8b26f1b25775bb8f3874f71a09c8480279d8705a
ahmadbahlool <billoahmad25@gmail.com>
2026-01-02 11:44:52 +0500

    add environment variables using python-decouple

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -1,6 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()
-import os
+from decouple import config
+
 """
 Django settings for saas project.
 
@@ -23,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9m+i9fjqo+_xniua#i(=7qn*5slqj%%5zfsxpy#)*fy)u*&pz8'
+SECRET_KEY=config("secret_key",cast="string")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("debug"))
+DEBUG = config("debug",cast=bool)
+
 print(DEBUG,type(DEBUG))
 
 
